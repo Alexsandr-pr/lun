518f3d7154ac308a435f88fc5e665a5d23a93818
Alex Pererodov <pererodovsasha2362000@gmail.com>
2024-05-27 11:37:38 +0300

    add change

--- src/js/app.js
+++ src/js/app.js
@@ -319,14 +319,15 @@ try {
 
 try {
 
+    const staticParent = document.querySelector(".static-game__block");
     const blockStatic = document.querySelector(".static-block").children;
 
     for(let i = 0; i < blockStatic.length; i++) {
         const block = blockStatic[i];
     
         block.addEventListener("click", () => {
-            const parent = block.parentElement;
-            parent.classList.toggle("_active")
+            
+            staticParent.classList.toggle("_active")
         })
     }
 }catch(e) {
@@ -795,11 +796,13 @@ function pushPlayers(playersArray, key, index) {
         bottom: ${+key + 71}px;    
     `
     }
+    let color = index === 0 ? "#115E2A" : "#FF4848"
     
 
     for(let i = 0; i < playersArray.length; i++) {
         const player = playersArray[i];
 
+        
         const div = document.createElement("div");
         div.classList.add("player");
 
@@ -808,7 +811,7 @@ function pushPlayers(playersArray, key, index) {
             <div class="player__block">
                 <span class="player__number">${player.PlayerNum}</span>
                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="24" viewBox="0 0 25 24" fill="none">
-                    <path class="player__bg" d="M1.9481 9.63337L1.51953 2.34766H23.3767V9.2048L19.9481 9.63337V22.0619H4.9481V9.63337H1.9481Z" fill="#115E2A"/>
+                    <path class="player__bg" d="M1.9481 9.63337L1.51953 2.34766H23.3767V9.2048L19.9481 9.63337V22.0619H4.9481V9.63337H1.9481Z" fill="${color}"/>
                     <path  d="M22.5499 0.950195H2.34581C1.84563 0.950056 1.36563 1.14745 1.01026 1.49943C0.654888 1.85141 0.452899 2.32949 0.448242 2.82964V8.64838C0.448242 9.6844 1.30643 10.5651 2.34581 10.5651H3.95135V21.6339C3.95135 22.6296 4.74461 23.4595 5.73681 23.4595H19.1266C19.6082 23.4532 20.0681 23.2586 20.408 22.9174C20.7479 22.5762 20.9408 22.1155 20.9452 21.6339V10.5651H22.5499C23.5893 10.5651 24.4483 9.68433 24.4483 8.64838V2.82964C24.4435 2.32939 24.2413 1.85129 23.8858 1.49933C23.5302 1.14737 23.0501 0.950015 22.5499 0.950195ZM15.313 2.73902C15.1319 3.3607 14.754 3.90686 14.2361 4.29547C13.7182 4.68408 13.0881 4.89415 12.4406 4.89415C11.7931 4.89415 11.1631 4.68408 10.6452 4.29547C10.1272 3.90686 9.74937 3.3607 9.56831 2.73902H15.313ZM22.6594 8.64838C22.6594 8.68378 22.5883 8.70175 22.5499 8.70175H20.0102C19.7661 8.70516 19.5328 8.80325 19.3596 8.97534C19.1864 9.14743 19.0868 9.38006 19.0818 9.62418V21.6707H5.74017V9.62418C5.7417 9.38296 5.6481 9.15086 5.47965 8.97819C5.31121 8.80552 5.0815 8.70619 4.84032 8.70175H2.34581C2.3075 8.70175 2.23706 8.68378 2.23706 8.64838V2.82964C2.23706 2.79901 2.30332 2.73902 2.34581 2.73902H7.71561C8.11273 5.04957 10.0804 6.70013 12.4411 6.70013C14.8018 6.70013 16.769 5.04957 17.1665 2.73902H22.5506C22.5932 2.73902 22.6602 2.79864 22.6602 2.82964L22.6594 8.64838Z" fill="white"/>
                 </svg>
                 <span class="player__minute">
