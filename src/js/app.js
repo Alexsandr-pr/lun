78d7048c4f87b85692b106b128544cb1a7966dfa
Alex Pererodov <pererodovsasha2362000@gmail.com>
2024-05-28 13:32:22 +0300

    add try/catch

--- src/js/app.js
+++ src/js/app.js
@@ -50,34 +50,42 @@ try {
 }
 
 
-const contentWrapper = document.querySelector(".dashboard__body-content");
-const dashboardTable = document.querySelectorAll(".dashboard__table");
+try {
+    const contentWrapper = document.querySelector(".dashboard__body-content");
+    const dashboardTable = document.querySelectorAll(".dashboard__table");
 
 
-function changeWidthWrapper() {
-    const width = dashboardTable[0].clientWidth * dashboardTable.length;
-    contentWrapper.style.width = `${width}px`;
-    console.log(contentWrapper.style.width)
+    function changeWidthWrapper() {
+        const width = dashboardTable[0].clientWidth * dashboardTable.length;
+        contentWrapper.style.width = `${width}px`;
+        console.log(contentWrapper.style.width)
+    }
+
+    window.addEventListener("resize", changeWidthWrapper);
+
+    changeWidthWrapper()
+
+    
+    $(function(){
+        $(".wrapper1").scroll(function(){
+            $(".wrapper2")
+                .scrollLeft($(".wrapper1").scrollLeft());
+        });
+        $(".wrapper2").scroll(function(){
+            $(".wrapper1")
+                .scrollLeft($(".wrapper2").scrollLeft());
+        });
+    });
+} catch(e) {
+
 }
 
-window.addEventListener("resize", changeWidthWrapper);
-
-changeWidthWrapper()
-
-$(function(){
-    $(".wrapper1").scroll(function(){
-        $(".wrapper2")
-            .scrollLeft($(".wrapper1").scrollLeft());
-    });
-    $(".wrapper2").scroll(function(){
-        $(".wrapper1")
-            .scrollLeft($(".wrapper2").scrollLeft());
-    });
-});
-
-
 import dashboardCreateLine from "./modules/dashboardPaintLines.js";
-dashboardCreateLine()
+try {
+    dashboardCreateLine()
+} catch(e) {
+
+}
 
 
 const animItems = document.querySelectorAll("._anim-items");
